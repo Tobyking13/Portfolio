d84aa526503f92ca429122176aff252a5f906033
Toby King <tobyking13@gmail.com>
2023-03-04 10:15:47 +0000

    adds a light/dark theme switch

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,31 +1,71 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NavTabs from './components/NavTabs';
-import Home from './components/pages/Home/Home';
-import About from './components/pages/About';
-import Contact from './components/pages/Contact';
-import Portfolio from './components/pages/Portfolio';
-import "./style.css"
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavTabs from "./components/NavTabs/NavTabs";
+import Home from "./components/pages/Home/Home";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+import Portfolio from "./components/pages/Portfolio";
+import styled, { ThemeProvider } from "styled-components";
+import MoonIcon from "./components/icons/moonIcon";
+import SunIcon from "./components/icons/sunIcon";
+import Switch from "./components/switch";
+
+import "./style.css";
+
+let StyledApp = styled.div`
+  min-height: 100vh;
+  background-color: ${(props) => props.theme.body};
+  color: ${(props) => props.theme.title};
+`;
+
+const darkTheme = {
+  body: "#1c1c1c",
+  title: "#fff",
+  subtitle: "#b6b6b6",
+};
+
+const lightTheme = {
+  body: "#fff",
+  title: "#1c1c1c",
+  subtitle: "#b6b6b6",
+};
 
 function App() {
+  const [theme, setTheme] = useState("dark");
+  const isDarkTheme = theme === "dark";
+
+  const toggleTheme = () => {
+    setTheme(isDarkTheme ? "light" : "dark");
+  };
+
   return (
-    <Router>
-      <div>
-        <NavTabs />
-        {/* Wrap Route elements in a Routes component */}
-        <Routes>
-          {/* Define routes using the Route component to render different page components at different paths */}
-          {/* Define a default route that will render the Home component */}
-          <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="portfolio" element={<Portfolio />} />
-          {/* Define a route that will have descendant routes */}
-          <Route path="contact/*" element={<Contact />} />
-        </Routes>
-      </div>
-    </Router>
+    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
+      <StyledApp>
+        <Router>
+          <div className="navbar">
+            <div className="theme-switch">
+            <SunIcon />
+            <Switch toggleTheme={toggleTheme} isDarkTheme={isDarkTheme} />
+            <MoonIcon />
+            </div>
+          
+            <NavTabs />
+            {/* Wrap Route elements in a Routes component */}
+            <Routes>
+              {/* Define routes using the Route component to render different page components at different paths */}
+              {/* Define a default route that will render the Home component */}
+              <Route path="/" element={<Home />} />
+              <Route path="about" element={<About />} />
+              <Route path="portfolio" element={<Portfolio />} />
+              {/* Define a route that will have descendant routes */}
+              <Route path="contact/*" element={<Contact />} />
+            </Routes>
+            
+          </div>
+        </Router>
+      </StyledApp>
+    </ThemeProvider>
   );
 }
 
 export default App;
-
---
+++ react-portfolio/src/components/pages/lightDark.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function lightDark() {
+    return
+}
+
+export default lightDark
