b1d9dfcc8a293663166189568a6ac247fa2a8a0a
Toby King <tobyking13@gmail.com>
2023-03-06 16:07:47 +0000

    adds working contact form

--- react-portfolio/src/pages/Contact/Contact.js
+++ react-portfolio/src/pages/Contact/Contact.js
@@ -11,7 +11,7 @@ function Contact() {
 
         <div className="col-4">
           <div className="contact-info">
-          <br></br>
+            {/* <br></br>
             <br></br>
             <h3 className="display-6">Email</h3>
             <br></br>
@@ -43,7 +43,53 @@ function Contact() {
                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
                 alt="LinkedIn"
               />
+            </a> */}
+            <div className="contact-form">
+              <form
+                action="https://getform.io/f/05330002-35c0-4ec1-8217-46dfad3de30c"
+                method="POST"
+              >
+                <label for="name">Name:</label>
+                <br></br>
+                <input type="text" name="name" id="name"/>
+                <br></br>
+                <label for="email">Email:</label>
+                <br></br>
+                <input type="email" name="email" id="email" />
+                <br></br>
+                <label for="message">Message:</label>
+                <br></br>
+                <textarea type="text" name="message" id="message" />
+                <input
+                  type="hidden"
+                  name="_gotcha"
+                  style={{ display: "none !important" }}
+                />
+                <br></br>
+                <button className="btn btn-outline-dark btn-lg contact-button" type="submit">Send</button>
+              </form>
+              <br></br>
+              <h3 className="display-6">Socials</h3>
+            <a
+              className="contact-icons"
+              href="https://www.linkedin.com/in/toby-king-951193266/"
+            >
+              <img
+                className="contact-icons"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg"
+                alt="LinkedIn"
+              />
             </a>
+            <br></br>
+            <br></br>
+            <a className="contact-icons" href="https://github.com/Tobyking13">
+              <img
+                className="contact-icons"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+                alt="LinkedIn"
+              />
+            </a>
+            </div>
           </div>
         </div>
         <div className="col-1">
@@ -51,12 +97,12 @@ function Contact() {
         </div>
 
         <div className="col-7">
-          <div style={{display:"flex"}}>
-          <iframe
-            className="cv"
-            src="https://drive.google.com/file/d/1kUosXMc5kJ1TBSMz_I1rVQR4W-L9k2Fz/preview?usp=sharing"
-            title="cv"
-          ></iframe>
+          <div style={{ display: "flex" }}>
+            <iframe
+              className="cv"
+              src="https://drive.google.com/file/d/1kUosXMc5kJ1TBSMz_I1rVQR4W-L9k2Fz/preview?usp=sharing"
+              title="cv"
+            ></iframe>
           </div>
         </div>
       </div>
